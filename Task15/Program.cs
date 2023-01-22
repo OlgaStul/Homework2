0ff05a5a06b14b83ac39cf1b9c1ff559c86f657a
Olga <chi89@yandex.ru>
2023-01-22 17:45:03 +0300

    является ли введеная цифра дня недели выходным днем

---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет   */
+
+Console.Write("Введите номер дня недели от 1 до 7: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+if(number == 1) Console.WriteLine("Понедельник - не выходной");
+if(number == 2) Console.WriteLine("Вторник - не выходной");
+if(number == 3) Console.WriteLine("Среда - не выходной");
+if(number == 4) Console.WriteLine("Четверг - не выходной");
+if(number == 5) Console.WriteLine("Пятница - не выходной");
+if(number == 6) Console.WriteLine("Суббота - выходной");
+if(number == 7) Console.WriteLine("Воскресенье - выходной");
+if(number > 7) Console.WriteLine("Такого дня нет");
